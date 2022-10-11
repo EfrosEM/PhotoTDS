7f703d7d61731a1f17237a476edded7d2d642f3d
EfrosEM <eduardoespinosamerono@gmail.com>
2022-10-11 18:44:25 +0200

    ventana perfil

---
+++ PhotoTDS/src/main/java/ventanas/PerfilUsuario.java
@@ -0,0 +1,111 @@
+package ventanas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+
+public class PerfilUsuario {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PerfilUsuario window = new PerfilUsuario();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PerfilUsuario() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(PerfilUsuario.class.getResource("/recursos/image.png")));
+		frame.setBounds(100, 100, 577, 616);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelNorte = new JPanel();
+		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
+		panelNorte.setLayout(new BoxLayout(panelNorte, BoxLayout.X_AXIS));
+		
+		JLabel lblNewLabel = new JLabel("PhotoTDS");
+		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 18));
+		panelNorte.add(lblNewLabel);
+		
+		Component rigidArea = Box.createRigidArea(new Dimension(50, 30));
+		panelNorte.add(rigidArea);
+		
+		JButton btnNewButton = new JButton("+");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setMaximumSize(new Dimension(20, 20));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		panelNorte.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("A+");
+		btnNewButton_1.setMaximumSize(new Dimension(20, 20));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panelNorte.add(btnNewButton_1);
+		
+		textField = new JTextField();
+		textField.setMaximumSize(new Dimension(2147483647, 20));
+		textField.setMinimumSize(new Dimension(7, 15));
+		textField.setPreferredSize(new Dimension(7, 15));
+		panelNorte.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(PerfilUsuario.class.getResource("/recursos/lupa.png")));
+		panelNorte.add(lblNewLabel_1);
+		
+		Component rigidArea_1 = Box.createRigidArea(new Dimension(50, 30));
+		panelNorte.add(rigidArea_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		panelNorte.add(btnNewButton_2);
+		
+		JPanel panelCentral = new JPanel();
+		frame.getContentPane().add(panelCentral, BorderLayout.CENTER);
+		
+		JPanel panelSur = new JPanel();
+		frame.getContentPane().add(panelSur, BorderLayout.SOUTH);
+	}
+
+}
