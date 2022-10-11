0b0c9c93d61555b8c9d2eb6d4182eed1a133823d
EfrosEM <eduardoespinosamerono@gmail.com>
2022-10-11 17:45:31 +0200

    clase notificacion

---
+++ PhotoTDS/src/main/java/Dominio/Notificacion.java
@@ -0,0 +1,30 @@
+package Dominio;
+
+import java.time.LocalDate;
+
+public class Notificacion {
+
+	private LocalDate fecha;
+	private Publicacion publicacion;
+	private Usuario user;
+	
+	public Notificacion(Usuario user, Publicacion publicacion) {
+		this.user = user;
+		this.publicacion = publicacion;
+		fecha = LocalDate.now();
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public Publicacion getPublicacion() {
+		return publicacion;
+	}
+
+	public Usuario getUser() {
+		return user;
+	}
+	
+	
+}
