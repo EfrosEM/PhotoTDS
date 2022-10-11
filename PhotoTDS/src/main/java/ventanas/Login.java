2a01f2a91a58e2abb5662742b813f68eecdd512b
AlexAriasJ <alexranero@gmail.com>
2022-10-11 19:09:03 +0200

    Se ha empezado la ventana Login.

--- PhotoTDS/src/main/java/ventanas/Login.java
+++ PhotoTDS/src/main/java/ventanas/Login.java
@@ -3,10 +3,26 @@ package ventanas;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
 
 public class Login {
 
 	private JFrame frame;
+	private JTextField textFieldUser;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -36,8 +52,25 @@ public class Login {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 380);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
+		JPanel panelTitulo = new JPanel();
+		panelTitulo.setMinimumSize(new Dimension(32767, 100));
+		panelTitulo.setMaximumSize(new Dimension(32767, 100));
+		panelTitulo.setPreferredSize(new Dimension(10, 80));
+		frame.getContentPane().add(panelTitulo);
+		panelTitulo.setLayout(new BoxLayout(panelTitulo, BoxLayout.Y_AXIS));
+		
+		Component rigidArea = Box.createRigidArea(new Dimension(25, 25));
+		panelTitulo.add(rigidArea);
+		
+		JLabel lblPhotoTDS = new JLabel("PhotoTDS");
+		lblPhotoTDS.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblPhotoTDS.setFont(new Font("Arial", Font.BOLD, 30));
+		panelTitulo.add(lblPhotoTDS);
+		
 	}
 
 }
