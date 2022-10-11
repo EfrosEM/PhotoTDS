567631119d743fd6a8dc951bc5123acba1cc636a
EfrosEM <eduardoespinosamerono@gmail.com>
2022-10-11 16:55:48 +0200

    commit de clase publicación

--- PhotoTDS/src/main/java/Dominio/Album.java
+++ PhotoTDS/src/main/java/Dominio/Album.java
@@ -3,33 +3,17 @@ package Dominio;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Album {
+public class Album extends Publicacion{
 
-	private String tematica;
 	private ArrayList<Foto> fotos;
-	private ArrayList<Hashtag> hashtags;
-	private Usuario user;
-	
-	public Album(String tematica, Usuario user) {
-		this.tematica = tematica;
-		this.user = user;
-		this.fotos = new ArrayList<Foto>();
-		this.hashtags = new ArrayList<Hashtag>();
-	}
 
-	public String getTematica() {
-		return tematica;
-	}
-	
-	public Usuario getUser() {
-		return user;
+	public Album(String titulo, String descripcion, Usuario user, Hashtag...hashtag) {
+		super(titulo, descripcion, user, hashtag);
+		this.fotos = new ArrayList<Foto>();
 	}
 
 	public List<Foto> getFotos() {
 		return new ArrayList<Foto>(fotos);
 	}
-	
-	public List<Hashtag> getHashtags() {
-		return new ArrayList<Hashtag>(hashtags);
-	}
+
 }
