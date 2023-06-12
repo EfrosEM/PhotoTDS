0afb86c0e26ed730ecc5326ff7a08b36538fb480
AlexAriasJ <alexranero@gmail.com>
2023-06-12 20:41:37 +0200

    Added test cases

--- PhotoTDS/src/test/java/dominio/AlbumTest.java
+++ PhotoTDS/src/test/java/dominio/AlbumTest.java
@@ -5,22 +5,54 @@ import static org.junit.Assert.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AlbumTest {
+	
+	private Usuario u1;
+	private Usuario u2;
+	private Foto foto;
+	private Album album;
+	private Notificacion notificacion;
 
-	@Test
-	public void testAddFoto() {
+	@Before
+	public void testAlbum() {
 		
-		ArrayList<Foto> fotos = new ArrayList<Foto>();
-		Foto foto = new Foto("ruta", "descripcion", 1, LocalDate.now());
-		fotos.add(foto);
+		u1 = new Usuario("user", "user1", "1", "user1@um.es", "test", null, "Usuario 1 test", "rutaFotoPerfil");
+		u2 = new Usuario("user", "user2", "2", "user2@um.es", "test", null, "Usuario 2 test", "rutaFotoPerfil");
 		
-		Album album = new Album("titulo", LocalDate.now(), null);
+		foto = new Foto("ruta", "descripcion", 1, LocalDate.now());
+		//fotos.add(foto);
+		
+		album = new Album("titulo", LocalDate.now());
 		album.addFoto(foto);
 		
+		notificacion = new Notificacion(u1, foto);
+		u2.addNotificacion(notificacion);
+		
+	}
+	
+	@Test
+	public void testAlbumNotNull() {
+		assertNotNull(album);
+	}
+	
+	@Test
+	public void testAddPhoto() {
+		ArrayList<Foto> fotos = new ArrayList<Foto>();
+		fotos.add(foto);
 		
 		assertEquals(fotos, album.getFotos());
 	}
+	
+	@Test
+	public void testNotification() {
+		ArrayList<Notificacion> notificaciones = new ArrayList<Notificacion>();
+		notificaciones.add(notificacion);
+		
+		assertEquals(u2.getNotificaciones(), notificaciones);
+	}
+	
 
 }
